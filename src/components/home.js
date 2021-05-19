6d0fddde4d9b05f81c3929f27692dd5fbbd4db5d
JoshuaLamke <51033351+JoshuaLamke@users.noreply.github.com>
2021-05-18 22:49:22 -0400

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,7 @@ import './home.css'
 import Me from '../img/me.PNG'
 let Home = () => {
     return (
-        <div className="container d-flex flex-column align-items-center justify-content-center h-100">
+        <div className="container d-flex flex-column align-items-center justify-content-center h-100 pt-3">
             <div className="d-flex flex-column align-items-center col-9 mb-3">
                 <h1>Welcome To My Portfolio!</h1>
                 <h2 style={{color: "#212121"}}>My name is Joshua Lamke and I am a senior studying computer science at GMU. I am passionate about web development and many other areas of computer science.</h2>
